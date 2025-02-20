f09346aba710137534fd98957a28976c00d7f1de
Hunter <muratoreh@gmail.com>
2025-02-19 19:43:36 -0500

    Added clickable teleport link for death location

--- src/main/java/com/kinggunch/keepxpmod/KeepXPMod.java
+++ src/main/java/com/kinggunch/keepxpmod/KeepXPMod.java
@@ -2,17 +2,21 @@ package com.kinggunch.keepxpmod;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.math.BlockPos;
 
 @Mod(modid = KeepXPMod.MODID, name = KeepXPMod.NAME, version = KeepXPMod.VERSION)
 public class KeepXPMod {
     public static final String MODID = "keepxpmod";
-    public static final String NAME = "Keep XP on Death Mod";
-    public static final String VERSION = "1.0";
+    public static final String NAME = "KeepXPMod - Death Location Link";
+    public static final String VERSION = "1.4";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
@@ -20,56 +24,24 @@ public class KeepXPMod {
     }
 
     @Mod.EventBusSubscriber
-    public static class XPHandler {
-        private static final String XP_TAG = "SavedXP";
-
-        // Prevent XP from dropping on death
-        @SubscribeEvent
-        public static void preventXPDrop(LivingExperienceDropEvent event) {
+    public static class DeathLocationHandler {
+        
+        @SubscribeEvent(priority = EventPriority.HIGHEST)
+        public static void onPlayerDeath(LivingDeathEvent event) {
             if (event.getEntity() instanceof EntityPlayer) {
-                event.setCanceled(true); // Stops XP orbs from dropping
-            }
-        }
+                EntityPlayer player = (EntityPlayer) event.getEntity();
+                BlockPos deathPos = player.getPosition();
 
-        // Save XP when the player dies
-        @SubscribeEvent
-        public static void onPlayerDeath(PlayerEvent.Clone event) {
-            if (event.isWasDeath()) { // Ensure this runs only on death, not dimension change
-                EntityPlayer original = event.getOriginal();
-                EntityPlayer newPlayer = event.getEntityPlayer();
+                // DEBUG: Log death position
+                System.out.println("[KeepXPMod] Saving Death Location: " + deathPos);
 
-                int xp = original.experienceTotal; // Get total XP before death
-                
-                // DEBUG LOGGING: Check XP before saving
-                System.out.println("Saving XP: " + xp);
+                // Create a clickable teleport message
+                String command = "/tp " + player.getName() + " " + deathPos.getX() + " " + deathPos.getY() + " " + deathPos.getZ();
+                TextComponentString message = new TextComponentString("Died again? Click to return to your death location :())");
+                message.setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command)));
 
-                // Save XP to player's persistent NBT data
-                NBTTagCompound playerData = newPlayer.getEntityData();
-                playerData.setInteger(XP_TAG, xp);
-            }
-        }
-
-        // Restore XP when the player logs in after dying
-        @SubscribeEvent
-        public static void onPlayerRespawn(PlayerEvent.Clone event) {
-            if (event.isWasDeath()) { // Ensure we are restoring only after death
-                EntityPlayer newPlayer = event.getEntityPlayer();
-                NBTTagCompound playerData = newPlayer.getEntityData();
-
-                if (playerData.hasKey(XP_TAG)) {
-                    int savedXP = playerData.getInteger(XP_TAG);
-
-                    // DEBUG LOGGING: Check XP before restoring
-                    System.out.println("Restoring XP: " + savedXP);
-
-                    // Restore XP properly
-                    newPlayer.addExperience(savedXP);
-
-                    // Clear stored XP after restoring
-                    playerData.removeTag(XP_TAG);
-                } else {
-                    System.out.println("No saved XP found!");
-                }
+                // Send the clickable message to the player
+                player.sendMessage(message);
             }
         }
     }
