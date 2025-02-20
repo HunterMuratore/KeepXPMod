222ee57838edfa1d2e04d546de0285d6317d07bb
Hunter <muratoreh@gmail.com>
2025-02-19 19:43:13 -0500

    revert main

--- src/main/java/com/kinggunch/keepxpmod/KeepXPMod.java
+++ src/main/java/com/kinggunch/keepxpmod/KeepXPMod.java
@@ -5,18 +5,17 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameType;
 
 @Mod(modid = KeepXPMod.MODID, name = KeepXPMod.NAME, version = KeepXPMod.VERSION)
 public class KeepXPMod {
     public static final String MODID = "keepxpmod";
-    public static final String NAME = "KeepXPMod - Death Location Link";
-    public static final String VERSION = "1.4";
+    public static final String NAME = "KeepXPMod - Respawn Location";
+    public static final String VERSION = "1.3";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
@@ -24,8 +23,10 @@ public class KeepXPMod {
     }
 
     @Mod.EventBusSubscriber
-    public static class DeathLocationHandler {
-        
+    public static class RespawnHandler {
+        private static final String DEATH_DATA = "KeepXPModData";
+
+        // Save player's exact death location on death
         @SubscribeEvent(priority = EventPriority.HIGHEST)
         public static void onPlayerDeath(LivingDeathEvent event) {
             if (event.getEntity() instanceof EntityPlayer) {
@@ -35,13 +36,43 @@ public class KeepXPMod {
                 // DEBUG: Log death position
                 System.out.println("[KeepXPMod] Saving Death Location: " + deathPos);
 
-                // Create a clickable teleport message
-                String command = "/tp " + player.getName() + " " + deathPos.getX() + " " + deathPos.getY() + " " + deathPos.getZ();
-                TextComponentString message = new TextComponentString("Died again? Click to return to your death location :())");
-                message.setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command)));
+                // Save death location in player's persistent NBT data
+                NBTTagCompound data = player.getEntityData();
+                NBTTagCompound deathData = new NBTTagCompound();
+                deathData.setInteger("SavedX", deathPos.getX());
+                deathData.setInteger("SavedY", deathPos.getY());
+                deathData.setInteger("SavedZ", deathPos.getZ());
+                data.setTag(DEATH_DATA, deathData);
+            }
+        }
 
-                // Send the clickable message to the player
-                player.sendMessage(message);
+        // Restore player to death location and set Creative mode on respawn
+        @SubscribeEvent(priority = EventPriority.HIGHEST)
+        public static void onPlayerRespawn(PlayerEvent.Clone event) {
+            if (event.isWasDeath()) { // Ensure this runs only on death, not dimension change
+                EntityPlayer newPlayer = event.getEntityPlayer();
+                NBTTagCompound data = newPlayer.getEntityData();
+
+                if (data.hasKey(DEATH_DATA)) {
+                    NBTTagCompound deathData = data.getCompoundTag(DEATH_DATA);
+                    int savedX = deathData.getInteger("SavedX");
+                    int savedY = deathData.getInteger("SavedY");
+                    int savedZ = deathData.getInteger("SavedZ");
+
+                    // DEBUG: Log teleportation location
+                    System.out.println("[KeepXPMod] Teleporting to: " + savedX + ", " + savedY + ", " + savedZ);
+
+                    // Teleport player to saved death location
+                    newPlayer.setPositionAndUpdate(savedX + 0.5, savedY, savedZ + 0.5);
+
+                    // Set player to creative mode to prevent instant death
+                    newPlayer.setGameType(GameType.CREATIVE);
+
+                    // Clear stored location after respawn
+                    data.removeTag(DEATH_DATA);
+                } else {
+                    System.out.println("[KeepXPMod] No saved location found, respawning normally.");
+                }
             }
         }
     }
